fb6e8fe3793f301bbd7dc9fd28d8b9cb420d771c
susFries <52565954+rein98111@users.noreply.github.com>
2026-04-19 23:20:24 +0800

    Add files via upload

---
+++ maps/tuto/map.js
@@ -0,0 +1,72 @@
+window.currentTrack = {
+    info: {
+        title: "TUTORIAL - City",
+        artist: "Gustaf Grefberg",
+        creator: "susFries",
+        level: "01",
+        audio: "maps/tuto/audio.mp3",
+        preview: "maps/tuto/audio.mp3",
+        cover: "maps/tuto/cover.png",
+        scrollSpeed: 0.0002 // 較慢的流速方便新手反應
+    },
+    notes: [
+        // --- 1 ---
+        { time: 2576, message: "玩家，歡迎來到AXIS！" },
+		{ time: 6478, message: "這是遊戲教學的譜面。" },
+		{ time: 10381, message: "在這裡，你可以學到如何玩這個遊戲。" },
+		{ time: 14283, message: "本遊戲沒有按鍵限制。" },
+		{ time: 18186, message: "鍵盤上的所有按鍵都可以點。" },
+		{ time: 22088, message: "首先，先讓我們學習如何「單擊」。" },
+		{ time: 25991, message: "圓圈與最中間的框框重疊時，按下去！" },
+		{ time: 29893, message: "讓我們開始吧！" },
+		
+		// --- 單擊 ---
+		{ time: 33295, angle: 270 },
+		{ time: 35247, angle: 90 },
+		{ time: 37198, angle: 180 },
+		{ time: 39149, angle: 0 },
+		{ time: 41100, angle: 225 },
+		{ time: 43052, angle: 45 },
+		{ time: 45003, angle: 315 },
+		{ time: 46954, angle: 135 },
+		
+		// --- 2 ---
+		{ time: 49405, message: "太棒了！你已經學會了「單擊」。" },
+		{ time: 53308, message: "現在，讓我們來學習「滑條」的按法。" },
+		{ time: 57210, message: "滑條到中間時，按住不要放，直到滑條結束再放掉。" },
+		{ time: 61113, message: "讓我們開始吧！" },
+		
+		// --- 滑條 ---
+		{ time: 64515, endTime: 66466, angle:270 , type: "hold" },
+		{ time: 68417, endTime: 70369, angle:90 , type: "hold" },
+		{ time: 72320, endTime: 74271, angle:180 , type: "hold" },
+		{ time: 76222, endTime: 78173, angle:0 , type: "hold" },
+		{ time: 80125, endTime: 82076, angle:225 , type: "hold" },
+		{ time: 84027, endTime: 85978, angle:45 , type: "hold" },
+		{ time: 87930, endTime: 89881, angle:315 , type: "hold" },
+		{ time: 91832, endTime: 93783, angle:135 , type: "hold" },
+		
+		// --- 3 ---
+		{ time: 96234, message: "恭喜你學會了基本玩法！" },
+		{ time: 100137, message: "每張譜面都會有不同的節奏表現方式。" },
+		{ time: 104039, message: "有可能還會出現同時兩顆的圓圈。" },
+		{ time: 107942, message: "現在，先來把這首歌打完吧！" },
+		
+		// --- 尾段 ---
+		{ time: 111344, angle: 270 },
+		{ time: 112320, angle: 90 },
+		{ time: 113295, angle: 180 },
+		{ time: 114393, angle: 0 },
+		{ time: 115247, endTime: 116222, angle:135 , type: "hold" },
+		{ time: 117198, endTime: 118173, angle:225 , type: "hold" },
+		{ time: 119149, angle: 0 },
+		{ time: 120125, angle: 90 },
+		{ time: 121100, angle: 180 },
+		{ time: 122076, angle: 270 },
+		{ time: 123052, endTime: 124027, angle:315 , type: "hold" },
+		{ time: 125093, endTime: 125978, angle:45 , type: "hold" },
+		{ time: 126966, message: "雙擊！！" },
+		{ time: 126954, angle: 225 },{ time: 126954, angle: 315 },
+		{ time: 130369, type: "end" },
+    ]
+};
